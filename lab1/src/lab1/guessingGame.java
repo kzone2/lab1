36a704b8aa75c3e5733632be9a05eab18f13540c
David Feldman <kzone2@gmail.com>
2014-09-25 12:54:00 -0500

    Initial Version

---
+++ lab1/src/lab1/guessingGame.java
@@ -0,0 +1,22 @@
+package lab1;
+
+import java.util.*;
+public class guessingGame {
+
+	public static void main(String[] args) {
+		Random rn = new Random();
+		int answer = rn.nextInt(10) + 1;
+		System.out.println("Guess a number between 1 and 10");
+		Scanner in = new Scanner(System.in);
+		
+		int guess = in.nextInt();
+		
+		if(guess == answer){
+			System.out.println("Correct, the answer is " + answer + " you have gambled and won!");
+		}
+		else{
+			System.out.println("There are forces working against you and you suck at life. Goodbye");
+		}	
+	}
+
+}
